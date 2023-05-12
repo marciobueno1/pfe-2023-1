980ffed9a843f1a803d9b19e0f1c82669bdf51cf
Márcio Bueno <marcio.bueno@unicap.br>
2023-05-11 21:42:09 -0300

    useReducer

--- app003/src/routes/bears.jsx
+++ app003/src/routes/bears.jsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
 import { useBearStore } from "../zustand";
+import { useEffect, useState, useReducer } from "react";
 
 const BearCounter = () => {
   const bears = useBearStore((state) => state.bears);
@@ -21,12 +22,69 @@ const Controls = () => {
   );
 };
 
+function reducer(state, action) {
+  if (action.type === "increment_counter") {
+    return {
+      ...state,
+      counter: state.counter + 1,
+    };
+  } else if (action.type === "increment_counter_by") {
+    return {
+      ...state,
+      counter: state.counter + action.payload,
+    };
+  }
+  throw Error("Unknown action.");
+}
+
 export const Bears = () => {
+  const [state, dispatch] = useReducer(reducer, { counter: 42, counter2: 100 });
+  const [counter, setCounter] = useState(0);
+  const [par, setPar] = useState(true);
+  const handleClick = () => {
+    setCounter((counter) => counter + 1);
+    setCounter((counter) => counter + 1);
+    setCounter((counter) => counter + 1);
+  };
+  useEffect(() => {
+    console.log("useEffect");
+    setPar(counter % 2 == 0);
+  }, [counter]);
   return (
     <>
       <BearCounter />
       <Controls />
       <Link to="/">Go to Home</Link>
+      <hr />
+      <h3>
+        {counter} - {par ? "par" : "ímpar"}
+      </h3>
+      <button onClick={handleClick}>Add</button>
+      <hr />
+      <h3>
+        {state.counter} - {par ? "par" : "ímpar"}
+        <br />
+        {state.counter2} - {par ? "par" : "ímpar"}
+      </h3>
+      <button
+        onClick={() =>
+          dispatch({
+            type: "increment_counter_by",
+            payload: 11,
+          })
+        }
+      >
+        Add1
+      </button>
+      <button
+        onClick={() =>
+          dispatch({
+            type: "increment_counter",
+          })
+        }
+      >
+        Add2
+      </button>
     </>
   );
 };
--- app003/src/zustand.js
+++ app003/src/zustand.js
@@ -1,9 +1,18 @@
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 
-export const useBearStore = create((set) => ({
-  bears: 0,
-  increasePopulation: () => set((state) => ({ bears: state.bears + 1 })),
-  removeAllBears: () => set({ bears: 0 }),
-  increasePopulationBy: (value) =>
-    set((state) => ({ bears: state.bears + value })),
-}));
+export const useBearStore = create(
+  persist(
+    (set) => ({
+      bears: 0,
+      dmds: 1,
+      increasePopulation: () => set((state) => ({ bears: state.bears + 1 })),
+      removeAllBears: () => set({ bears: 0 }),
+      increasePopulationBy: (value) =>
+        set((state) => ({ bears: state.bears + value })),
+    }),
+    {
+      name: "food-storage", // unique name
+    }
+  )
+);
